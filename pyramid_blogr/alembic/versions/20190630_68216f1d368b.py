8d1cb48fd2d3f5c459d320faa33c334008d4af20
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-06-30 19:07:55 -0700

    first commit

---
+++ pyramid_blogr/alembic/versions/20190630_68216f1d368b.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 68216f1d368b
+Revises: 
+Create Date: 2019-06-30 18:28:00.560225
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '68216f1d368b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
---
+++ pyramid_blogr/models/user.py
@@ -0,0 +1,16 @@
+import datetime
+from pyramid_blogr.models.meta import Base
+from sqlalchemy import(
+    Column, 
+    Integer, 
+    Unicode, 
+    UnicodeText, 
+    DateTime
+)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    password = Column(Unicode(255), nullable=False)
+    last_logged = Column(DateTime, default=datetime.datetime.utcnow)
