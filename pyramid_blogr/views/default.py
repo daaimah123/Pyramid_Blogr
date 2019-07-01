c3bb0562244eb33c5d53fdf1bcec0439fee83974
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-06-30 20:56:36 -0700

    prepare initial views

--- pyramid_blogr/views/default.py
+++ pyramid_blogr/views/default.py
@@ -1,32 +1,13 @@
 from pyramid.view import view_config
-from pyramid.response import Response
-
-from sqlalchemy.exc import DBAPIError
-
-from .. import models
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(models.User)
-        one = query.filter(models.User.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'pyramid_blogr'}
+@view_config(route_name='home', 
+            renderer='pyramid_blogr:templates/index.jinja2')
+def index_page(request):
+    return {}
 
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for descriptions and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+@view_config(route_name='auth', match_param='action=in', renderer='string',
+             request_method='POST')
+@view_config(route_name='auth', match_param='action=out', renderer='string')
+def sign_in_out(request):
+    return {}
